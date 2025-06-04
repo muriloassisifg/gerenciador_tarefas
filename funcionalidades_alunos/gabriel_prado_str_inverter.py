a654db90b6cf477c59784a02ea437397811a1b61
Gabriel-de-Souza-Prado <souza.prado@estudantes.ifg.edu.br>
2025-06-04 20:22:26 -0300

    caixa de ferramentas

---
+++ funcionalidades_alunos/gabriel_prado_str_inverter.py
@@ -0,0 +1,9 @@
+def executar():
+
+    texto = input("Por favor, digite um texto: ")
+    texto_invertido = texto[::-1]
+    print(f"Texto invertido: {texto_invertido}")
+
+if __name__ == '__main__':
+    executar()
+    
