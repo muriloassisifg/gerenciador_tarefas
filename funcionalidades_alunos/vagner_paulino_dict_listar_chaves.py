9f3a7bc341b792fd35e3bf4508ec8bd9b7c77fc8
Vagner Paulino <devagnerlp@gmail.com>
2025-06-11 19:36:18 -0300

    commit 11-06

---
+++ funcionalidades_alunos/vagner_paulino_dict_listar_chaves.py
@@ -0,0 +1,9 @@
+
+def executar():
+    dicionario= {'id': 1, 'produto': 'Notebook Dell', 'valor': '4.990'}
+    print()
+    print (dicionario.keys())
+    print ('Obrigado')
+    print()
+if __name__ == "__main__":
+    executar()
