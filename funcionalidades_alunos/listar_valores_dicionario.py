f966170efe93528eadc2cc8ebe6d8af685d77ddc
Warley Lemos <wa.rley46@gmail.com>
2025-06-11 20:00:15 -0300

    Adiciona funcionalidade para listar todos os valores de um dicionário.

---
+++ funcionalidades_alunos/listar_valores_dicionario.py
@@ -0,0 +1,8 @@
+def executar():
+    dados = {
+        "produto": "Notebook",
+        "preço": 4500,
+        "marca": "MarcaX"
+    }
+    valores = list(dados.values())
+    print(f"Os valores do dicionário são: {valores}")
