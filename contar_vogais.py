9bfb5dd29d5be7c66154e45247c287d180c086dc
Andre <a.jarczewski@estudantes.ifg.edu.br>
2025-06-11 19:44:18 -0300

    Atualização dia 11/06/25

--- contar_vogais.py
+++ contar_vogais.py
@@ -1,10 +1,11 @@
 def executar():
     """Contagem de vogais em String"""
     vogais = "aeiouAEIOU"
-    texto = input("""/n Contador de vogal 
+    texto = input("""\n Contador de vogal 
 Texto:""")
     contador = sum( 1 for letra in texto if letra in vogais)
     if contador >= 1:
-        print (f" O texto -{texto}- possui {contador} vogais")
+        print (f" O texto '{texto}' possui {contador} vogais")
     else:
-        print (f" O texto -{texto}- não possui vogais")
+        print (f" O texto '{texto}' não possui vogais")
+
